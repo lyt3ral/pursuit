1c5925e4dd40c9f9f56db341199f726c25b0b5e3
lyt3ral <saisabarishmail@gmail.com>
2025-07-17 23:33:35 +0530

    feat: added application model

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, serial, integer, text, timestamp } from 'drizzle-orm/pg-core';
+import { pgTable, integer, text, timestamp, pgEnum } from 'drizzle-orm/pg-core';
 
 export const user = pgTable('user', {
 	id: text('id').primaryKey(),
@@ -13,6 +13,29 @@ export const session = pgTable('session', {
 		.notNull()
 		.references(() => user.id),
 	expiresAt: timestamp('expires_at', { withTimezone: true, mode: 'date' }).notNull()
+});
+
+export const jobTypeEnum = pgEnum('job_type', ['REMOTE', 'ONSITE', 'HYBRID']);
+
+export const applicationStatusEnum = pgEnum('application_status', [
+	'APPLIED',
+	'INTERVIEW',
+	'OFFER',
+	'REJECTED'
+]);
+
+export const application = pgTable('job_application', {
+	id: text('id').primaryKey(),
+	userId: text('user_id')
+		.notNull()
+		.references(() => user.id),
+	role: text('role').notNull(),
+	company: text('company').notNull(),
+	type: jobTypeEnum('type').notNull(),
+	location: text('location'),
+	appliedAt: timestamp('applied_at', { withTimezone: true, mode: 'date' }).notNull().defaultNow(),
+	notes: text('notes'),
+	status: applicationStatusEnum('status').notNull()
 });
 
 export type Session = typeof session.$inferSelect;
